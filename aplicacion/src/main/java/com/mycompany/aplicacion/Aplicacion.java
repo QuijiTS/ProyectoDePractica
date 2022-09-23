a446b0c700ce0d9fc11701cc9861a2fbb170b1d9
Jose <quiji33@gmail.com>
2022-09-22 21:08:12 -0600

    Aplicacion

---
+++ aplicacion/src/main/java/com/mycompany/aplicacion/Aplicacion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.aplicacion;
+
+/**
+ *
+ * @author quiji
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        DBConnection db = new DBConnection();
+        db.createConnection();
+        
+        Login l1 = new Login();
+        l1.setVisible(true);
+        
+        MenuAdministrador m1 = new MenuAdministrador();
+        m1.setVisible(true);
+        
+        Eliminar i1 = new Eliminar();
+        i1.setVisible(false);
+    }
+}
